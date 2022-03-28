4ffb025030f1c40bc5e1c20a9089ce874ddb6081
mahsyazeta <zetadhana@gmail.com>
2022-03-29 01:25:49 +0700

    update Prak4

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:jbnavigation/models/item.dart';
 
 class ItemPage extends StatelessWidget {
+  const ItemPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final Item itemArgs = ModalRoute.of(context)?.settings.arguments as Item;
+    return Scaffold(
+      body: Container(
+      
+        child: Text(
+          itemArgs.name + ' with ' + itemArgs.price.toString(),
+          textAlign: TextAlign.center,
+        )
+      ),
+    );
   }
   
 }
